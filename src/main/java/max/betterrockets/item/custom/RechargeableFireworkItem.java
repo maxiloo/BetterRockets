28a53d47d7e91537e1f045f9099ca701e7f8b26d
Max Unterbusch <82529938+maxiloo@users.noreply.github.com>
2024-08-20 00:57:34 +0200

    improve: boosting behavior

--- src/main/java/max/betterrockets/item/custom/RechargeableFireworkItem.java
+++ src/main/java/max/betterrockets/item/custom/RechargeableFireworkItem.java
@@ -1,5 +1,6 @@
 package max.betterrockets.item.custom;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -9,6 +10,10 @@ import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
 public class RechargeableFireworkItem extends Item {
+    private static final double MAX_VELOCITY = 4;
+    private static final double VELOCITY_INCREMENT = 0.3;
+    private static final int MAX_USE_TIME = 10;
+    private static final int COOLDOWN = 30;
     private static final int MAX_CHARGE = 64;
     private int charge;
 
@@ -21,12 +26,34 @@ public class RechargeableFireworkItem extends Item {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack itemStack = user.getStackInHand(hand);
 
-        if (!world.isClient) {
-            user.addVelocity(user.getRotationVector().x * 1.5, user.getRotationVector().y * 1.5, user.getRotationVector().z * 1.5);
-            user.velocityModified = true; // Mark velocity as modified
-
-            user.getItemCooldownManager().set(this, 10);
+        if (!world.isClient && user.isFallFlying()) {
+            user.setCurrentHand(hand);
+            user.getItemCooldownManager().set(this, COOLDOWN);
         }
         return new TypedActionResult<>(ActionResult.SUCCESS, itemStack);
     }
+
+    @Override
+    public void usageTick(World world, LivingEntity user, ItemStack stack, int remainingUseTicks) {
+        super.usageTick(world, user, stack, remainingUseTicks);
+        if (user.isFallFlying()) {
+            boostPlayer((PlayerEntity) user);
+        }
+    }
+
+    @Override
+    public int getMaxUseTime(ItemStack stack, LivingEntity user) {
+        return MAX_USE_TIME;
+    }
+
+    private void boostPlayer(PlayerEntity user) {
+
+        double velocity = user.getVelocity().length();
+
+        if (velocity < MAX_VELOCITY) {
+            user.addVelocity(user.getRotationVector().x * VELOCITY_INCREMENT, user.getRotationVector().y * VELOCITY_INCREMENT, user.getRotationVector().z * VELOCITY_INCREMENT);
+            user.velocityModified = true; // Mark velocity as modified
+        }
+    }
+
 }
